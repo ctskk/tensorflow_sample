713844232d3ca418c4f1710ec9c26e9aa6b600dc
konno <ctskk.1974@gmail.com>
2017-12-03 22:20:47 +0900

    pylint対応, グラフ出力

--- 01.minimum/test.py
+++ 01.minimum/test.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# pylint: disable=C0103,C0111,C0325
+
 import tensorflow as tf
 
 hello = tf.constant('Hello, TensorFlow!')
@@ -6,3 +11,5 @@ print(sess.run(hello))
 a = tf.constant(10)
 b = tf.constant(32)
 print(sess.run(a + b))
+
+tf.summary.FileWriter('./', sess.graph)
